5dbdeca5a03455cb81719425f6e091b9ac32094f
dhruv <haldardhruv@gmail.com>
2024-09-09 17:12:42 +0530

    update content-blog.js, content-minimal.js and content.js

--- src/app/resources/content-blog.js
+++ src/app/resources/content-blog.js
@@ -9,7 +9,7 @@ const person = {
     },
     role:      'Aerospace Engineer',
     avatar:    '/images/avatar.jpg',
-    // location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Swedish']  // optional: Leave the array empty if you don't want to display languages
 }
 
